f72be581193c974f0652dc90f3b3c155e5835eed
Mark Norman Francis <norm@cackhanded.net>
2013-10-20 09:49:48 +0100

    Homepage should show today's event

--- london/homepage/views.py
+++ london/homepage/views.py
@@ -10,7 +10,7 @@ class HomepageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomepageView, self).get_context_data(**kwargs)
         context['next_events'] = Event.objects.filter(
-                date__gt = datetime.date.today()
+                date__gte = datetime.date.today()
             ).order_by(
                 'date'
             )[:5]
