3b4595e55a9535a1c833764ea13085654961d854
Murray Steele <muz@h-lame.com>
2015-08-17 14:12:40 +0100

    Make time_until in PSEvent cope with 0 days
    When relativedelta reports 0 days it might mean that it's exactly 1 month until the next event, not that it's only a few hours away because relativedelta rolls day differences of more than a month up into the months attribute (ditto for more months than a year).  To solve this we collapse down any attribute from the relative date that is 0 and then combine the rest as a sentence.
    
    We've also moved the formatting into a global helper in util.py in case it's useful elsewhere.

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import pytz
+import inflect
 
+p = inflect.engine()
 
 def combine_tz(date, time, tzinfo):
     dt = datetime.combine(date, time)
@@ -8,3 +10,16 @@ def combine_tz(date, time, tzinfo):
 
 def utc_now():
     return datetime.utcnow().replace(tzinfo=pytz.UTC)
+
+def format_relative_time(relative):
+    date_part_names = 'year month day hour minute'.split()
+
+    date_parts = zip(date_part_names, [getattr(relative, date_part+'s') for date_part in date_part_names])
+    formatted = [p.no(date_part, value) for (date_part, value) in date_parts if value]
+
+    rest, tail = formatted[:-1], formatted[-1]
+
+    if rest:
+        return u'In %s, and %s' % ( ', '.join(rest), tail )
+    else:
+        return u'In %s' % ( tail )
