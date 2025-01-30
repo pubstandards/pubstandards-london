5f46425145cbdb68cc033e390aa7fda67c2c7842
Russ Garrett <russ@garrett.co.uk>
2025-01-30 17:34:41 +0000

    Update IRC command

--- tools/ps_irc_command.py
+++ tools/ps_irc_command.py
@@ -15,21 +15,33 @@ data = httpx.get("https://london.pubstandards.com/next.json").json()
 
 event = None
 
+now = arrow.now()
+
 for e in data:
     datetime = arrow.get(e["date"] + " " + e["time"])
     event = e
-    if datetime.date() >= arrow.now().date():
+    if datetime.date() >= now.date():
         break
 
 if event is None:
     print("No upcoming events.")
     exit()
 
-if datetime < arrow.now():
+rsvp = event['rsvp_responses']
+
+if datetime < now:
     msg = f"{event['title']} is happening right now at {event['location']}. Get to the pub!"
 else:
-    friendly_date = datetime.humanize(granularity=["day", "hour"])
-    msg = f"{event['title']} is happening at {event['location']} {friendly_date}: "
+    friendly_date = datetime.humanize(now)
+    msg = f"{event['title']} is happening at {event['location']} {friendly_date}. "
+    if rsvp['attending'] > 0:
+        msg += f"{rsvp['attending']} people are coming"
+    if rsvp['interested'] > 0:
+        if rsvp['attending'] > 0:
+            msg += ' and '
+        msg += f"{rsvp['interested']} people are interested"
+    if rsvp['attending'] > 0 or rsvp['interested'] > 0:
+        msg += ": "
     msg += f"https://london.pubstandards.com/event/{event['slug']}"
 
-print(msg.encode("utf-8"))
+print(msg)
